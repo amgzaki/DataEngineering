50b32c478675ead098066d3b33d251cf28d9da68
amgzak1 <amgad.zaki@exfo.com>
2020-12-10 05:59:21 -0500

    Pushing changes for the PostrgreSQL project

--- 1_Data_Modeling_with_PostgreSQL/etl.py
+++ 1_Data_Modeling_with_PostgreSQL/etl.py
@@ -7,37 +7,39 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+#     # insert song record
+    song_data = df[['song_id','title','artist_id','year','duration']].values.tolist()
+    for song in song_data: #in case there is more than one song in each file
+        cur.execute(song_table_insert, song)
     
-    # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+#     # insert artist record
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values.tolist()
+    for artist in artist_data: #in case there is more than one artist in each file
+        cur.execute(artist_table_insert, artist)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == "NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df.ts, unit='ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ['timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,13 +47,17 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (index, pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
